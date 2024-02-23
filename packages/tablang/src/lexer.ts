5a502ebf8aa50acaa02dca8feff880172b34dcfc
abdessamad-zgor <abdessamadzgor@gmail.com>
2024-02-23 05:39:45 +0100

    astro docs

--- packages/tablang/src/lexer.ts
+++ packages/tablang/src/lexer.ts
@@ -16,12 +16,28 @@ enum TokenType {
   EOF
 }
 
+const keywords = new Map<string, TokenType>([
+  ["and", TokenType.AND],
+  ["or", TokenType.OR],
+  ["if", TokenType.IF],
+  ["else", TokenType.ELSE],
+  ["print", TokenType.PRINT],
+  ["true", TokenType.TRUE],
+  ["false", TokenType.FALSE],
+  ["while", TokenType.WHILE],
+  ["let", TokenType.LET],
+  ["alert", TokenType.ALERT],
+  ["nil", TokenType.NIL],
+  ["for", TokenType.FOR],
+  ["fun", TokenType.FUN]
+])
+
 class Token {
   type: TokenType
-  lexeme: string
+  lexeme: string|number
   line: number
 
-  constructor(type: TokenType, lexeme: string, line: number){
+  constructor(type: TokenType, lexeme: string|number, line: number){
     this.line = line
     this.type = type
     this.lexeme = lexeme
@@ -110,16 +126,79 @@ export function lexer( input: string): Token[] {
           new Token(TokenType.EQUAL_EQUAL, '==', line):
           new Token(TokenType.EQUAL, '=', line))
         break;
+      case '"':
+        while (peek(input, position) !='"' && position<input.length) {
+          if(peek(input, position)=='\n') line++;
+          position++
+        }
+
+        if(position>=input.length) throw Error('unterminated string')
+        position++;
+
+        tokens.push(
+          new Token(TokenType.STR, input.slice(start+1, position-1),line)
+        )
+        break;
       default:
-        
+        if(isDigit(c)){
+          while(isDigit(peek(input, position))) position++
+          if(peek(input, position)=="."&&isDigit(peekNext(input, position))) position++
+          while(isDigit(peek(input, position))) position++
+          tokens.push(
+            new Token(
+              TokenType.NUM,
+              parseNumber(input.slice(start, position+1)),
+              line)
+          )
+        } else if(isAlpha(c)){
+          while(isAlphaNumeric(peek(input, position))) position++
+          let token = input.slice(start, position+1)
+          let type = keywords.get(token);
+          
+          tokens.push(
+            new Token(
+              type?type:TokenType.IDENTIFIER,
+              token,
+              line)
+          )
+        }
     }
   }
 
   return tokens
 }
 
+function parseNumber(ch: string): number{
+  if(ch.includes('.')) return parseFloat(ch)
+  return parseInt(ch)
+}
+
 function match(input:string, position: number,  char: string): boolean {
   if(position>=input.length) return false;
   else if(input[position]!=char) return false;
   return true
+}
+
+function peek(input: string, position: number): string|undefined {
+  if(position>=input.length-1) return;
+  return input[position]
+}
+
+function peekNext(input: string, position: number): string|undefined {
+  if(position>=input.length-1) return;
+  return input[position+1]
+}
+
+function isDigit(ch: string): boolean {
+  return "0123456789".includes(ch)
+}
+
+function isAlpha(c: string): boolean{
+  return (c >= 'a' && c <= 'z') ||
+    (c >= 'A' && c <= 'Z') ||
+    c == '_';
+}
+
+function isAlphaNumeric(c: string) {
+  return isDigit(c) || isAlpha(c)
 }
